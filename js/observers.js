c973e5e9c3201c412c873e0176c3af1356b96a37
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-06-08 17:13:04 -0500

    fade and slide. really cool effect

--- js/observers.js
+++ js/observers.js
@@ -1,7 +1,9 @@
 const DOM = (function () {
     const elements = {
         header: document.querySelector("header"),
-        firstSection: document.querySelector(".home-intro")
+        firstSection: document.querySelector(".home-intro"),
+        faders: document.querySelectorAll(".fade-in"), //NEW
+        sliders: document.querySelectorAll(".slide-in")
     }
     return elements;
 })();
@@ -22,3 +24,27 @@ const observerFirstSection = new IntersectionObserver(function (entries, observe
 }, optionsFirstSection);
 
 observerFirstSection.observe(DOM.firstSection);
+
+// NEW:
+const optionsAppear = {
+    threshold: 0,
+    rootMargin: "0px 0px -250px 0px"
+};
+const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
+    entries.forEach((currentEntry) => {
+        // console.log(currentEntry);
+        if (currentEntry.isIntersecting) {
+            currentEntry.target.classList.add("appear");
+            appearOnScroll.unobserve(currentEntry.target); // to stop observing once observed
+        }
+    })
+}, optionsAppear)
+
+// Souvenez-vous comment créer un observeur pour beaucoup d'éléments?
+DOM.faders.forEach((currentElement) => {
+    appearOnScroll.observe(currentElement);
+});
+
+DOM.sliders.forEach((currentElement) => {
+    appearOnScroll.observe(currentElement);
+})
